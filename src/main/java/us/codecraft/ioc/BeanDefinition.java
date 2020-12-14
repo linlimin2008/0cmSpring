7b1e81f8743be66ff33fe4f214ae017720df3f54
0cm <linlimin@zealfi.com>
2020-12-14 10:27:08 +0800

    Fix

---
+++ src/main/java/us/codecraft/ioc/BeanDefinition.java
@@ -0,0 +1,19 @@
+package us.codecraft.ioc;
+
+/**
+ * @功能:
+ * @项目名:0cmSpring
+ * @作者:0cm
+ * @日期:2020/12/1410:25 上午
+ */
+public class BeanDefinition {
+    private Object bean;
+
+    public BeanDefinition (Object bena){
+        this.bean = bean;
+    }
+
+    public Object getBean(){
+        return bean;
+    }
+}
